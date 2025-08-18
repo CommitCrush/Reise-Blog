6c42b99cb1f4704c6a9476f4483fe424c153039b
hana <hanaabrham89@gmail.com>
2025-08-18 23:02:27 +0200

    feat: implement createPost action and define Post model schema

--- nextjs_basic/src/actions/blog.ts
+++ nextjs_basic/src/actions/blog.ts
@@ -1,19 +1,11 @@
 "use server";
 
+
 import Post from "@/models/Post";
 
-export async function createPost({
-    title,
-    content,
-    author,
-    imageUrl,
-}: {
-    title: string;
-    content: string;
-    author?: string;
-    imageUrl?: string;
-}) {
-    const post = new Post({ title, content, author, imageUrl });
-    await post.save();
-    return post;
+export async function createPost({ title, content, author }: { title: string; content: string; author?: string }) {
+	
+	const post = new Post({ title, content, author });
+	await post.save();
+	return post;
 }
