c3434c3221f927e65f56cd490a0f09de0eeacf88
Roznrasho <roznrasho@gmail.com>
2025-08-18 23:18:24 +0200

    feat: add getSessionUser function to retrieve current user from session

---
+++ nextjs_basic/src/lib/auth.ts
@@ -0,0 +1,20 @@
+import { connectDB } from "@/lib/db";
+import { User } from "@/models/user";
+import { cookies } from "next/headers";
+
+// Hole den aktuellen Benutzer aus der Session
+export async function getSessionUser() {
+    const cookieStore = await cookies();
+    const sessionToken = cookieStore.get("sessionToken");
+
+    // Wenn kein Session-Token vorhanden ist, gebe null zurück
+    if (!sessionToken) return null;
+
+    // Verbindung zur Datenbank herstellen
+    await connectDB();
+    const user = await User.findById(sessionToken?.value).lean();
+
+    // Wenn kein Benutzer gefunden wurde, gebe null zurück
+    if (!user) return null;
+    return user || null;
+}
