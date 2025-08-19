c857fcdce55422a2580b3b765331e6ee494b8d18
hana <hanaabrham89@gmail.com>
2025-08-19 11:30:50 +0200

    feat: implement create, read, update, and delete functionality for blog posts

--- nextjs_basic/src/app/blog/[id]/page.tsx
+++ nextjs_basic/src/app/blog/[id]/page.tsx
@@ -1,18 +1,15 @@
-import { getComments, getPostById } from "@/actions/blog";
-import { getSessionUser } from "@/lib/auth";
-import CommentBox from "@/app/_componennts/CommentBox";
-import LikeButton from "@/app/_componennts/LikeButton";
+import { getPostById } from "@/actions/blog";
 
-export default async function BlogDetailPage({
-  params,
-}: {
+interface BlogDetailPageProps {
   params: { id: string };
-}) {
-  const post = await getPostById(params.id);
-  const comments = await getComments(params.id);
-  const user = await getSessionUser();
+}
 
-  if (!post) return <div>Post nicht gefunden.</div>;
+export default async function BlogDetailPage({ params }: BlogDetailPageProps) {
+  const post = await getPostById(params.id);
+
+  if (!post) {
+    return <div>Post nicht gefunden.</div>;
+  }
 
   return (
     <div style={{ maxWidth: 700, margin: "2rem auto" }}>
@@ -28,43 +25,6 @@ export default async function BlogDetailPage({
       <div style={{ color: "#888", marginTop: 16 }}>
         Erstellt am: {new Date(post.createdAt).toLocaleString()}
       </div>
-      <div style={{ marginTop: 16 }}>
-        {user ? (
-          <LikeButton
-            postId={post._id.toString()}
-            userId={user._id.toString()}
-            initialLikes={post.likes || []}
-          />
-        ) : (
-          <>
-            <strong>Likes:</strong> {post.likes?.length || 0}
-          </>
-        )}
-      </div>
-      <div style={{ marginTop: 16 }}>
-        <strong>Kommentare:</strong> {comments.length}
-        <ul>
-          {comments.map((c: any) => (
-            <li key={c._id}>
-              <b>{c.user?.username || "Unbekannt"}</b>: {c.text}
-            </li>
-          ))}
-        </ul>
-        {user && (
-          <CommentBox
-            postId={post._id.toString()}
-            userId={user._id.toString()}
-            onCommentAdded={async () => {}}
-          />
-        )}
-      </div>
-      <p style={{ color: "#888" }}>
-        von: <b>
-          {typeof post.author === "object" && "username" in post.author
-            ? (post.author as { username: string }).username
-            : "Unbekannt"}
-        </b>
-      </p>
-    </div>
+          </div>
   );
 }
