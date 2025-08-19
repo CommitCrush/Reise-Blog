6b3ca61de0a403dc48c01cb9d66316724ec3fae9
hana <hanaabrham89@gmail.com>
2025-08-19 09:33:57 +0200

    feat: add imageUrl field to Post model and update createPost function

--- nextjs_basic/src/models/Post.ts
+++ nextjs_basic/src/models/Post.ts
@@ -4,6 +4,7 @@ export interface IPost extends Document {
 	title: string;
 	content: string;
 	author: mongoose.Types.ObjectId;
+	imageUrl?: string;
 	createdAt: Date;
 	updatedAt: Date;
 }
@@ -23,6 +24,10 @@ const postSchema = new Schema<IPost>(
 			ref: "User",
 			required: true,
 		},
+		   imageUrl: {
+        type: String,
+        required: false,
+    },
 	},
 	{
 		timestamps: true,
