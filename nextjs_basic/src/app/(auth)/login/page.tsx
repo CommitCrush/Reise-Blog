97681de43d47816cb56a7c38c7956f45dd2966f1
Roznrasho <roznrasho@gmail.com>
2025-08-19 18:49:55 +0200

    feat: implement login page with email and password fields

---
+++ nextjs_basic/src/app/(auth)/login/page.tsx
@@ -0,0 +1,37 @@
+import Link from "next/link";
+import { LoginUser } from "../../../actions/login";
+
+export default function LoginPage() {
+  return (
+    <main className="mx-auto h-screen flex items-center justify-center flex-col gap-5">
+      <h3 className=" text-xl text-center">Login</h3>
+      <form
+        //method="POST"
+        action={LoginUser}
+      >
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            name="email"
+            placeholder="email"
+            type="email"
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            name="password"
+            placeholder="password"
+            type="password"
+            required
+          />
+        </div>
+        <button type="submit">send</button>
+        <Link href="/register">Register</Link>
+      </form>
+    </main>
+  );
+}
