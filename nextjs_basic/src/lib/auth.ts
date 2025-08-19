917722fea25dd7aa508216c436170607e406d838
hana <hanaabrham89@gmail.com>
2025-08-19 09:11:49 +0200

    fix: correct import path for User model in auth.ts

--- nextjs_basic/src/lib/auth.ts
+++ nextjs_basic/src/lib/auth.ts
@@ -1,5 +1,5 @@
 import { connectDB } from "@/lib/db";
-import { User } from "@/models/user";
+import { User } from "@/models/User";
 import { cookies } from "next/headers";
 
 // Hole den aktuellen Benutzer aus der Session
